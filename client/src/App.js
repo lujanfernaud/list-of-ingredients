337450e74bb36fdd4dbb42f9761661a6c6acaffc
lujanfernaud <lujanfernaud@gmail.com>
2018-10-26 08:01:48 +0100

    Implement new App.js using React Router

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './components/Home'
+import NotFound from './components/NotFound'
+
+class App extends Component {
+  render () {
+    return (
+      <Router>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
+    )
+  }
+}
+
+export default App
